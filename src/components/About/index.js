8995be044adf30c922ab56cc6270cbe38a6adf53
Duncan Byrne <duncanbyrne69@outlook.com>
2022-02-28 12:20:24 -0500

    added more information to the about me section again

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,12 +9,14 @@ function About() {
         </div>
         <p>
         My name is Duncan Byrne and I am a web developer from Long Island, New York. I have been working with JavaScript developing projects for two years. I recently graduated from
-        a boot camp with Columbia University where I learned a full MERN stack along with the best practices. I had a great time learning and collaborating with the profeesor and 
-        fellow students. I am looking to join a company where I can continue to learn and grow as a developer in this field. If you wish to contact me my e-mail address is @duncanbyrne69@outlook.com. 
-
-        </p>
+        a boot camp with Columbia University where I learned a full MERN stack along with the best common practices. I had a great time learning and collaborating with the profeesor and 
+        fellow students. I have also completed almost two years at Suffolk County Community College. I have not finished my degree yet, I stopped because of the global pandemic in 2020 
+        and I found what I love to do in that time and I want to make the most of it and find the right company for me. 
+      </p>
         <p>
-          
+          I am looking to join a company where I can continue to learn and grow as a developer, I believe that in this field the best people to learn from are those with experience. 
+          If you want to contact me my e-mail adress is duncanbyrne69@outllok.com, you can also reach out to me on LinkedIn through the button on the bottom of the page. Thank you for 
+          checking out my portfolio, have a great day and I hope to hear from you soon!
         </p>
       </div>
     </section>
